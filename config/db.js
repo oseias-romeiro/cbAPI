94584d525d89de156efce0a7773f41d6196865b9
Oseias Romeiro <78980902+oseias-romeiro@users.noreply.github.com>
2021-11-16 22:55:13 -0300

    db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost/projeto'
+    // db: 'mongodb://localhost/projeto'
+    db: 'mongodb+srv://user-ex-node:alqpzm-GCAm7-1029@cluster0.8c2la.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
